429d5e2c33b2595f031ad29faee9faaef1770fe1
Aleia Knight <aleiaknight@gmail.com>
2019-10-16 11:26:13 -0700

    events displaying on homepage

--- app.py
+++ app.py
@@ -12,38 +12,50 @@ app = Flask(__name__)
 client = MongoClient()
 db=client.Eventuu  
 events = db.events
+events.drop()
 
+# √
 @app.route('/')
 def test():
-    return render_template('index.html', msg='Hiiiii')
+    return render_template('index.html', msg='Hiiiii', events=events.find())
 
-# VIEWING cart homepage
-@app.route('/event')
-def event_display():
-    return render_template('index.html', events=events.find())
+# # VIEWING event homepage
+# @app.route('/event')
+# def event_display():
+#     return render_template('index.html', events=events.find())
 
 
-# add event page
+
+# √
+# ADD A NEW EVENT
 @app.route('/event/add')
 def event_add():
+    '''go to event creation form'''
     return render_template('event_add.html')
 
-# add event
-@app.route('/event/submit', methods=['POST'])
-def event_submit():
+
+
+
+# VIEW A CREATED EVENT
+@app.route('/event/detail', methods=['POST'])
+def event_detail():
     event={
         'title': request.form.get('title'),
         'description': request.form.get('description')
     }
     event_id = events.insert_one(event).inserted_id
-    return redirect(url_for('event_view', event_id=event_id))
+    return redirect(url_for('test', event_id=event_id))
 
-# Route to VIEW ONE event
-@app.route('/event/<event_id>')
-def event_show(event_id):
-    """Show a one event"""
-    event = events.find_one({'_id': ObjectId(event_id)})
-    return render_template('event_view.html', event=event, events=events)
+
+
+# # Route to VIEW ONE event
+# @app.route('/event/detail/<event_id>')
+# def event_show_detail(event_id):
+#     """Show a one event"""
+#     event = events.find_one({'_id': ObjectId(event_id)})
+#     return render_template('index.html', event=event, events=events)
+
+
 
 
 
